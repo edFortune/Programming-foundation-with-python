f63ff0a1ecb8bc6965081bb22a36309ec211a2b9
edFortune <fortune.edmael1@gmail.com>
2017-04-11 18:34:19 -0500

    Update Mini-Project.py
    Fix the bug from translate function and change the directory to pics folder

--- Use functions/Mini-Project.py
+++ Use functions/Mini-Project.py
@@ -1,11 +1,17 @@
 import os
+from string import digits
 
 
 def rename_files():
     file_list = os.listdir("pics")
     print(file_list)
     
-
+    save_path = os.getcwd()
+    os.chdir(save_path+"\pics")
+    
     for file_name in file_list:
-        os.rename(file_name, file_name.translate(None, "0123456789"))
+        os.rename(file_name, file_name.translate(str.maketrans('', '', digits)))
+        
+    os.chdir(save_path)
+    
 rename_files()
